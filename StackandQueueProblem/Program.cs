a02acd19e11184a5a824bd3b7dcb1a75cbab0e2d
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-10 19:26:06 +0530

    [Vedhashni] Updated the file with stack and queue problem

---
+++ StackandQueueProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueueProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack and queue program");
+        }
+    }
+}
